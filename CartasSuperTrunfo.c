ec39d9a022b1c400b401406555f491fa1ba22ac9
mefiussuifem <matheuscmbrandao@gmail.com>
2025-10-30 00:42:57 +0000

    Densidade demografica e PIP per capita feitos.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,11 +12,14 @@ int main() {
     char cidade1 [50], cidade2 [50];
     int populacao1, ponto_turistico1, populacao2, ponto_turistico2;
     float area1, area2, pib1, pib2;
+    float densidade1, densidade2, pib_capita1, pib_capita2;
 
   // Área para entrada de dados
    /*----------CARTA 1-----------*/
 
     printf("Dados da Carta 1 \n");
+    printf("\n");
+
     printf("Digite o Estado: \n");
     scanf("%c", &estado1);
 
@@ -39,8 +42,9 @@ int main() {
     scanf("%d", &ponto_turistico1);
 
     /*-------------CARTA 2----------------*/
-
+    printf("\n");
     printf("Dados da Carta 2 \n");
+
     printf("Digite o Estado: \n");
     scanf(" %c", &estado2);
 
@@ -66,7 +70,8 @@ int main() {
 
   /*--------------------EXIBE CARTAS-----------------*/
 
-    printf("Abaixo as cartas digitadas:\n");
+printf("Abaixo as cartas digitadas:\n");
+    printf("\n");
     printf("Carta 1: \n");
     printf("Estado: %c \n", estado1);
     printf("Codigo: %s \n", codigo1);
@@ -75,6 +80,10 @@ int main() {
     printf("Area: %.2f km² \n", area1);
     printf("PIB: R$ %.2f bilhões de reais \n", pib1);
     printf("Numero de Pontos Turisticos: %d \n", ponto_turistico1);
+    densidade1 = (float) populacao1 / area1;
+    printf("Densidade Populacional: %.2f hab/km² \n", densidade1);
+    pib_capita1 = (float) pib1 / populacao1;
+    printf("PIB per Capita: %.2f reais \n", pib_capita1);
     printf("\n");
 
     printf("Carta 2: \n");
@@ -85,6 +94,10 @@ int main() {
     printf("Area: %.2f km² \n", area2);
     printf("PIB: R$ %.2f bilhões de reais \n", pib2);
     printf("Numero de Pontos Turisticos: %d", ponto_turistico2);
+    densidade2 = (float) populacao2 / area2;
+    printf("Densidade Populacional: %.2f hab/km² \n", densidade2);
+    pib_capita2 = (float) pib2 / populacao2;
+    printf("PIB per Capita: %.2f reais \n", pib_capita2);
 
 return 0;
 } 
